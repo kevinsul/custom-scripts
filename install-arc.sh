14b8a30bbe6a2eb294b115feca5519ed0b6e6aa6
kevinsul <kevinsul@live.com>
2019-12-17 15:43:12 -0600

    Create install-arc.sh

---
+++ install-arc.sh
@@ -0,0 +1,13 @@
+# Download the installation package
+wget https://aka.ms/azcmagent -O ~/Install_linux_azcmagent.sh
+
+# Install the connected machine agent. Omit the '--proxy "{proxy-url}"' parameters if proxy is not needed
+bash ~/Install_linux_azcmagent.sh
+
+azcmagent connect \
+  --service-principal-id "b8b0feed-07b3-42c6-b547-274b1ed4bba6" \
+  --service-principal-secret "d76da177-c9d9-4c30-80d4-88c10917597a" \
+  --resource-group "arc-rg1" \
+  --tenant-id "72f988bf-86f1-41af-91ab-2d7cd011db47" \
+  --location "westus2" \
+  --subscription-id "793b3d21-28ab-48d5-a41f-d842b7091c1c"
